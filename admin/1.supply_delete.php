76f924a20be5852c6093885783ddeff8a740046f
NATHANIEl90 <108976497+NATHANIEl90@users.noreply.github.com>
2024-09-21 18:11:54 +0800

    Add files via upload

---
+++ admin/1.supply_delete.php
@@ -0,0 +1,14 @@
+<?php
+include("../config/config.php");
+
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+    $delete_sql = "DELETE FROM supply_office WHERE id = $id";
+
+    if (mysqli_query($conn, $delete_sql)) {
+        echo 'Record deleted successfully.';
+    } else {
+        echo 'Unable to delete record. Please try again.';
+    }
+}
+?>
